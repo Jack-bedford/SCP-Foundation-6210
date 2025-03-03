35ee3e96b0e8329fa5c4a495865d95c62d537c8a
Jack-bedford <30072288@gmail.com>
2025-03-04 11:48:58 +1300

    QuickFix

--- src/NavMenu.jsx
+++ src/NavMenu.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { scps } from './scp.js';
-import Home from './home.jsx';
-
 
 function NavMenu() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light sticky-top bg-dark border-bottom text-white">
-      <div className="container-fluid ">
+      <div className="container-fluid">
         <Link className="navbar-brand text-white" to="/">SCP Foundation</Link>
 
-
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <span className="navbar-toggler-icon"></span>
+    </button>
 
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-          {scps && scps.length > 0 ? (  // ✅ Prevents errors if `scps` is undefined
+            {scps && scps.length > 0 ? (
               scps.map(scp => (
                 <li key={scp.Name} className="nav-item">
-                  <Link to={`/scp/${scp.Name}`} className="nav-link text-white">
+                  <NavLink 
+                    to={`/scp/${scp.Name}`} 
+                    className="nav-link text-white" 
+                    activeClassName="active"
+                  >
                     {scp.Class} - {scp.Name}
-                  </Link>
+                  </NavLink>
                 </li>
               ))
             ) : (
-              <li className="nav-item text-white">Loading SCPs...</li>
+              <li className="nav-item text-white">
+                <div className="spinner-border text-white" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              </li>
             )}
           </ul>
         </div>
